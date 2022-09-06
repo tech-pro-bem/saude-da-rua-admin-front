f1b147c5aa9882386c5cbaa0ff3aee6ef6559865
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-06 10:16:57 -0300

    fix: reimplementa aviso de erro de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import eyeClosedIcon from '../assets/eye-off.svg';
 import eyeOpenIcon from '../assets/eye-on.svg';
-import isValidEmail from '../utils/isValidEmail';
 
 function Login() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   /* Login error message */
-  const [hasLogginErrorOccurred, setHasLogginErrorOccurred] = useState(false);
-  useEffect(() => {
-    if (hasLogginErrorOccurred) {
-      setTimeout(() => {
-        setHasLogginErrorOccurred(false);
-      }, 5000); // 5 seconds
-    }
-  }, [hasLogginErrorOccurred]);
+  const [hasLoginErrorOccurred, setHasLoginErrorOccurred] = useState(false);
 
   /* useForm setup */
   const formInitialState = { email: '', password: '' };
   const {
     register,
     handleSubmit,
-    formState: { errors },
   } = useForm({ defaultValues: formInitialState });
 
   function onSubmit(data) {
     alert(JSON.stringify(data));
-    setHasLogginErrorOccurred(true);
+    setHasLoginErrorOccurred(true);
   }
 
   return (
@@ -44,18 +35,12 @@ function Login() {
               name="email"
               placeholder="Digite o e-mail cadastrado"
               className={`px-6 py-3 bg-transparent rounded-lg text-primary-black placeholder:text-medium-grey
-                outline outline-1 ${errors.email ? 'outline-error' : 'outline-primary-black'}`}
+                outline outline-1 ${hasLoginErrorOccurred ? 'outline-error' : 'outline-primary-black'}`}
               // eslint-disable-next-line react/jsx-props-no-spreading
               {...register('email', {
                 required: true,
-                validate: isValidEmail,
               })}
             />
-            {errors.email && (
-              <span className="mt-1 text-sm text-error">
-                {errors.email.type === 'required' ? 'O campo de e-mail é obrigatório' : 'Formato de e-mail inválido'}
-              </span>
-            )}
           </label>
           {/* Password Form Control */}
           <label htmlFor="password" className="flex flex-col mb-5">
@@ -67,7 +52,7 @@ function Login() {
                 name="password"
                 placeholder="Digite a sua senha"
                 className={`w-full px-6 py-3 pr-14 bg-transparent rounded-lg text-primary-black placeholder:text-medium-grey
-                outline outline-1 ${errors.password ? 'outline-error' : 'outline-primary-black'}`}
+                outline outline-1 ${hasLoginErrorOccurred ? 'outline-error' : 'outline-primary-black'}`}
                 // eslint-disable-next-line react/jsx-props-no-spreading
                 {...register('password', {
                   required: {
@@ -99,9 +84,9 @@ function Login() {
                 </button>
               </div>
             </div>
-            {errors.password && (
+            {hasLoginErrorOccurred && (
               <span className="mt-1 text-sm text-error">
-                {errors.password.message}
+                E-mail e/ou senha incorretos
               </span>
             )}
           </label>
@@ -118,12 +103,6 @@ function Login() {
           </div>
         </form>
       </section>
-      {/* Login error message */}
-      {hasLogginErrorOccurred && (
-        <div className="py-3 px-20 w-full max-w-lg absolute bottom-24 bg-error text-white rounded mt-10 flex justify-center">
-          <span className="text-center">E-mail e/ou senha incorretos</span>
-        </div>
-      )}
     </main>
   );
 }
