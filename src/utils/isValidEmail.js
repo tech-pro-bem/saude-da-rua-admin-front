44a17c3a7ba5fb8a71600bca06b37bc757bff028
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-09-06 10:29:28 -0300

    chore: remover arquivo de validação de email

--- src/utils/isValidEmail.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-useless-escape
-const isValidEmail = (email) => /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-
-export default isValidEmail;
