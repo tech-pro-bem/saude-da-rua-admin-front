57867e41e62f390036e4d18ffa1493a56c6d3a99
Angélica Silva <angelica.silva9323@gmail.com>
2022-09-25 15:38:43 -0300

    refactor: Modifica / para Login e adiciona o /home

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
