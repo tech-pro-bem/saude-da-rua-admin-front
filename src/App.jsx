9d8dc60c09ef3a5a4a80fb9c19b30dd6933d2435
Angélica Silva <angelica.silva9323@gmail.com>
2022-10-03 21:54:10 -0300

    refactor: Voltando rotas para estrutura anterior

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
