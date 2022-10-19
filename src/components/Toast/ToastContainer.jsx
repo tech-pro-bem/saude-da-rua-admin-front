97025144ba51e15515eabcd31c8b955202c9f876
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-10-19 14:59:25 -0300

    feat: adiciona implementação para Toast e ToastContainer

---
+++ src/components/Toast/ToastContainer.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import { useToast } from '../../contexts/toastContext';
+import Toast from './Toast';
+
+function ToastContainer() {
+  const { toastList } = useToast();
+
+  return (
+    <div className="w-[1346px] fixed top-0 right-0 z-10 overflow-hidden">
+      <div className="w-full flex flex-col gap-4">
+        {toastList.map((toast) => <Toast key={toast.id} toast={toast} />)}
+      </div>
+    </div>
+  );
+}
+export default ToastContainer;
