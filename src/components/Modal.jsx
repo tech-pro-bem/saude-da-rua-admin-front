c67e2a871765f93e2511fc6057a87a0eeaeaee60
matheusbarcellosdev <matheusbarcellos61@gmail.com>
2022-11-03 11:41:50 -0300

    :ok_hand: Alterações de code review

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import Trash from '../assets/trash.svg';
 
-function ModalDelete() {
+function ModalDelete({
+  text,
+  onCommit,
+  confirmationTitle = 'Confirmar Ação',
+  cancelButtonText = 'Cancelar',
+  confirmationButtonText = 'Confirmar',
+}) {
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -14,13 +21,13 @@ function ModalDelete() {
       <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[555px] bg-base shadow-lg">
         <Dialog.Title className="bg-primary-black py-4 px-6">
           <h2 className="text-[22px] font-semibold text-[#FBFBFB] leading-[33px]">
-            Confirmar ação
+            { confirmationTitle }
           </h2>
         </Dialog.Title>
 
         <Dialog.Description className="p-6">
           <p className="text-[18px] leading-[27px] pb-6">
-            Deseja mesmo realizar essa exclusão?
+            { text }
           </p>
           <div className="flex items-center justify-center gap-4">
             <Dialog.Close asChild>
@@ -28,14 +35,15 @@ function ModalDelete() {
                 type="button"
                 className="py-3 px-16 border-[1.5px] h-12 border-primary-black rounded-full text-primary-black font-bold"
               >
-                Cancelar
+                { cancelButtonText }
               </button>
             </Dialog.Close>
             <button
               type="button"
               className="py-3 px-16 border-[1.5px] h-12 bg-error border-error rounded-full text-base font-bold"
+              onClick={onCommit}
             >
-              Sim, excluir
+              { confirmationButtonText }
 
             </button>
           </div>
