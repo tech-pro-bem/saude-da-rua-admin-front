380737f03395914c86a74e728bed5e94377c0621
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-08 14:49:41 -0300

    refactor: implementa alterações no Modal e Sidebar

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
-import Trash from '../assets/trash.svg';
+import React from 'react';
 
 function ModalDelete({
   text,
@@ -9,45 +8,36 @@ function ModalDelete({
   confirmationTitle = 'Confirmar Ação',
   cancelButtonText = 'Cancelar',
   confirmationButtonText = 'Confirmar',
+  open,
+  setOpen,
 }) {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <button type="button">
-          <img src={Trash} alt="trash" />
-        </button>
-      </Dialog.Trigger>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Overlay className="fixed inset-0 bg-primary-black/20 backdrop-blur-sm" />
       <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[555px] bg-base shadow-lg">
-        <Dialog.Title className="bg-primary-black py-4 px-6">
-          <h2 className="text-[22px] font-semibold text-[#FBFBFB] leading-[33px]">
-            { confirmationTitle }
-          </h2>
+        <Dialog.Title className="bg-primary-black py-4 px-6 text-[22px] font-semibold text-[#FBFBFB] leading-[33px]">
+          {confirmationTitle}
         </Dialog.Title>
-
-        <Dialog.Description className="p-6">
-          <p className="text-[18px] leading-[27px] pb-6">
-            { text }
-          </p>
-          <div className="flex items-center justify-center gap-4">
-            <Dialog.Close asChild>
-              <button
-                type="button"
-                className="py-3 px-16 border-[1.5px] h-12 border-primary-black rounded-full text-primary-black font-bold"
-              >
-                { cancelButtonText }
-              </button>
-            </Dialog.Close>
+        <Dialog.Description className="p-6 text-[18px] leading-[27px]">
+          {text}
+        </Dialog.Description>
+        <div className="flex items-center justify-center gap-4 pb-6">
+          <Dialog.Close asChild>
             <button
               type="button"
-              className="py-3 px-16 border-[1.5px] h-12 bg-error border-error rounded-full text-base font-bold"
-              onClick={onCommit}
+              className="py-3 px-16 border-[1.5px] h-12 border-primary-black rounded-full text-primary-black font-bold"
             >
-              { confirmationButtonText }
-
+              {cancelButtonText}
             </button>
-          </div>
-        </Dialog.Description>
+          </Dialog.Close>
+          <button
+            type="button"
+            className="py-3 px-16 border-[1.5px] h-12 bg-error border-error rounded-full text-base font-bold"
+            onClick={onCommit}
+          >
+            {confirmationButtonText}
+          </button>
+        </div>
       </Dialog.Content>
     </Dialog.Root>
   );
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import File from '../../assets/file.svg';
 import Home from '../../assets/home.svg';
 import List from '../../assets/list.svg';
-import File from '../../assets/file.svg';
-import User from '../../assets/user.svg';
-import Settings from '../../assets/settings.svg';
 import Logout from '../../assets/logout.svg';
+import Settings from '../../assets/settings.svg';
+import User from '../../assets/user.svg';
 
 function SideBar() {
   const [open, setOpen] = useState(false);
@@ -21,10 +22,10 @@ function SideBar() {
     {
       name: 'Planilha de voluntários',
       icon: List,
-      path: '/planilha',
+      path: '/voluntarios',
     },
     {
-      name: ' Relatórios financeiros',
+      name: 'Relatórios financeiros',
       icon: File,
       path: '/relatorios',
     },
@@ -36,7 +37,7 @@ function SideBar() {
     {
       name: 'Configurações da conta',
       icon: Settings,
-      path: '/congifuracoes',
+      path: '/configuracoes',
     },
     {
       name: 'Sair',
@@ -51,7 +52,7 @@ function SideBar() {
       className={` ${
         open ? 'w-[291px]' : 'w-[94px]'
       } flex bg-base h-screen duration-500 transition-all fixed left-0
-      shadow-[0_4px_4px_rgba(16,24,40,0.1)]
+      shadow-[0_4px_4px_rgba(16,24,40,0.1)] z-[9999]
       `}
       aria-label="Sidebar"
     >
@@ -61,10 +62,9 @@ function SideBar() {
           onMouseEnter={() => setOpen(true)}
           onMouseLeave={() => setOpen(false)}
         >
-          {Menus.map((menu) => (
-            <Link to={menu.path}>
+          {Menus.map((menu, index) => (
+            <Link to={menu.path} key={index}>
               <li
-                key={menu.title}
                 className={`flex items-center mb-9 ml-0.5 px-8 py-3 rounded-lg hover:bg-light-blue ${
                   router.pathname === menu.path && 'bg-light-blue'
                 }`}
