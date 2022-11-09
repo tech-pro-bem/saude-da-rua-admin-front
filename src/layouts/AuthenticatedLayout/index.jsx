d237cd77bcccca1dab6e1e0929868cffe841086b
anajuliarauber <anajuliarauber@hotmail.com>
2022-11-09 08:35:59 -0300

    feat: adiciona logica para proteção de rotas autenticadas

--- src/layouts/AuthenticatedLayout/index.jsx
+++ src/layouts/AuthenticatedLayout/index.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../../components/SideBar/SideBar';
+import { getSessionStorage } from '../../utils/sessionStorage';
 
 function AuthenticatedLayout({ children }) {
+  const navigate = useNavigate();
+
+  const isTokenExpired = (token) => {
+    const tokenExpirationTime = jwtDecode(token).exp * 1000;
+    const currentTime = new Date().getTime();
+    const isExpired = (tokenExpirationTime - currentTime) < 0;
+    return isExpired;
+  };
+
+  useEffect(() => {
+    const token = getSessionStorage('token');
+    const isToken = !!token;
+
+    if (!isToken) {
+      navigate('/login');
+    }
+
+    if (isToken) {
+      if (isTokenExpired(token)) {
+        navigate('/login');
+      }
+    }
+  }, []);
   return (
     <div className="flex">
       <div className="mr-[205px]">
