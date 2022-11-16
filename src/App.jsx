83952c760de188049ee6286f68886a0c65eba973
matheusbarcellosdev <matheusbarcellos61@gmail.com>
2022-11-15 21:06:02 -0300

    Feat: Add rota temporaria para pagina de detalhes voluntario

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastProvider } from './contexts/toastContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import VoluteerDetails from './pages/VolunteerDetails';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          {/* Rota temporaria, ate que rota da tabela de voluntarios nao estiver pronta */}
+          <Route path="/:id" element={<VoluteerDetails />} />
         </Routes>
       </BrowserRouter>
     </ToastProvider>
