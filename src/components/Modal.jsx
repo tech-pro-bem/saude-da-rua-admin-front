a8bcbeb7b175beec0ce9d4c25ea3a583045f5984
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-16 10:44:56 -0300

    refactor: adapta Modal para reutilização

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,8 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import React from 'react';
 
-function ModalDelete({
-  text,
+function Modal({
+  children,
   onCommit,
   confirmationTitle = 'Confirmar Ação',
   cancelButtonText = 'Cancelar',
@@ -12,14 +12,14 @@ function ModalDelete({
   setOpen,
 }) {
   return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
+    <Dialog.Root open={open} onOpenChange={setOpen} className="z-[9999px]">
       <Dialog.Overlay className="fixed inset-0 bg-primary-black/20 backdrop-blur-sm" />
       <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[555px] bg-base shadow-lg">
         <Dialog.Title className="bg-primary-black py-4 px-6 text-[22px] font-semibold text-[#FBFBFB] leading-[33px]">
           {confirmationTitle}
         </Dialog.Title>
         <Dialog.Description className="p-6 text-[18px] leading-[27px]">
-          {text}
+          {children}
         </Dialog.Description>
         <div className="flex items-center justify-center gap-4 pb-6">
           <Dialog.Close asChild>
@@ -43,4 +43,4 @@ function ModalDelete({
   );
 }
 
-export default ModalDelete;
+export default Modal;
