cbc37ee8affb6f8e95dd31e66f9170dd90e0bfa4
Kleverton Oliveira <kleverton.oliveira.al@gmail.com>
2022-11-28 12:26:31 -0300

    chore: remove volunteersContext e uso no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastProvider } from './contexts/toastContext';
-import { VolunteersProvider } from './contexts/volunteersContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import PixKey from './pages/PixKey';
@@ -17,11 +16,7 @@ function App() {
           <Route path="/chave-pix" element={<PixKey />} />
           <Route
             path="/voluntarios"
-            element={(
-              <VolunteersProvider>
-                <Volunteers />
-              </VolunteersProvider>
-            )}
+            element={<Volunteers />}
           />
         </Routes>
       </BrowserRouter>
