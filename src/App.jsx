cf2433c163097f98d819825415b8db9af926e2d7
Lucas <lucas-m@outlook.com>
2022-12-15 03:41:14 -0300

    feat(financial-reports-page): add page route

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PixKey from './pages/PixKey';
 import Volunteers from './pages/Volunteers';
 import Medicines from './pages/Medicines';
 import MedicineDetails from './pages/MedicineDetails';
+import FinancialReports from './pages/FinancialReports';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/chave-pix" element={<PixKey />} />
             <Route path="/medicamentos" element={<Medicines />} />
             <Route path="/medicamentos/:id" element={<MedicineDetails />} />
+            <Route path="/relatorios" element={<FinancialReports />} />
             <Route
               path="/voluntarios"
               element={<Volunteers />}
